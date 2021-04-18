7cbd3b2b23527f9128c662eab3b22438f09b355c
trivernis <trivernis@protonmail.com>
2021-04-18 12:03:18 +0200

    Add crates.io metadata
    Signed-off-by: trivernis <trivernis@protonmail.com>

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -3,6 +3,26 @@ use crate::parsing::video_information::parse_video_information;
 use crate::types::VideoInformation;
 
 /// Returns information about a video
+/// ```
+/// use youtube_metadata::get_video_information;
+/// let information = get_video_information("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+///      .await
+///      .unwrap();
+///  assert_eq!(information.id, "dQw4w9WgXcQ".to_string());
+///  assert_eq!(
+///      information.url,
+///      "https://www.youtube.com/watch?v=dQw4w9WgXcQ".to_string()
+///  );
+///  assert_eq!(information.uploader, "RickAstleyVEVO".to_string());
+///  assert_eq!(
+///      information.title,
+///      "Rick Astley - Never Gonna Give You Up (Video)".to_string()
+///  );
+///  assert_eq!(
+///      information.thumbnail,
+///      Some("https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg".to_string())
+///  );
+/// ```
 pub async fn get_video_information(url: &str) -> YoutubeResult<VideoInformation> {
     let response = reqwest::get(url).await?;
     let response_text = response.text().await?;
